77dad1312b1b25b39e3ca00758ea57e72086139e
APavlides <alex__pavlides@hotmail.com>
2024-08-22 20:53:32 +0100

    feat: add app

---
+++ app/myapp.py
@@ -0,0 +1,159 @@
+import streamlit as st
+
+from query import main
+import asyncio
+import os
+
+
+if "run_once" not in st.session_state:
+    st.session_state["run_once"] = True
+
+# Specify the directory containing the text files
+TEXT_FILES_DIR = "./input/"
+
+# Get a list of all .txt files in the directory
+txt_files = [f for f in os.listdir(TEXT_FILES_DIR) if f.endswith(".txt")]
+
+
+# Cache the main function's response using the st.cache_data decorator
+@st.cache_data(show_spinner=False)
+def get_cached_response(question):
+    return asyncio.run(main(question=question, mock=False))
+
+
+# Tab selector
+tab = st.sidebar.radio("Select a tab", ["Search Documents", "View Documents"])
+
+
+if tab == "Search Documents":
+
+    # Adding a title to the main area
+    st.title("Q&A Patient History")
+
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    if st.session_state["run_once"] == True:
+        st.session_state["run_once"] = False
+
+        # Init questions
+
+        # user_q_list = ["What drugs has the patient been prescribed previously if known",
+        #                "What is the age of the patient if known",
+        #                "Who does the patient live with if known",
+        #                "What is their early development history if known i.e. traumas, significant events, what school they went to.",
+        #                "Why is the patient coming to the clinic?",
+        #                "Have social services been involved or has the patient had other early help",
+        #                "What is the patient's condition history.",
+        #                "What does the patient want?"]
+        user_q_list = [
+            "What drugs has the patient been prescribed previously if known",
+            "What treatment has the patient received previously.",
+            "What is the age, or data of birth (DOB), of the patient if known",
+        ]
+
+        for q in user_q_list:
+
+            # Display user message in chat message container
+            with st.chat_message("user"):
+                st.markdown(q)
+            # Add user message to chat history
+            st.session_state.messages.append({"role": "user", "content": q})
+
+            # response = get_cached_response(prompt)
+            output = "This is just a test"
+
+            # try:
+            #     with st.spinner("Model is working on it..."):
+            #         result = get_cached_response(question=q)
+            #         output = result.response
+            #         # st.subheader(f":blue[{i}]")
+            #         # st.write(reponse.response)
+            # except Exception as e:
+            #     st.error(f"An error occurred: {e}")
+            #     st.error(
+            #         "Oops, the GPT response resulted in an error :( Please try again with a different question."
+            #     )
+            # Display assistant response in chat message container
+            with st.chat_message("assistant"):
+                st.markdown(output)
+            # Add assistant response to chat history
+            st.session_state.messages.append({"role": "assistant", "content": output})
+
+    if prompt := st.chat_input("Ask additional questions..."):
+        # Display user message in chat message container
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+        # response = get_cached_response(prompt)
+        output = "This is just a test"
+
+        # try:
+        #     with st.spinner("Model is working on it..."):
+        #         result = get_cached_response(question=prompt)
+        #         output = result.response
+        #         # st.subheader(f":blue[{i}]")
+        #         # st.write(reponse.response)
+        # except Exception as e:
+        #     st.error(f"An error occurred: {e}")
+        #     st.error(
+        #         "Oops, the GPT response resulted in an error :( Please try again with a different question."
+        #     )
+
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            st.markdown(output)
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": output})  #
+
+    # Function to format the chat history into a string
+    def format_messages_for_text(messages):
+        formatted = []
+        for msg in messages:
+            role = "User" if msg["role"] == "user" else "Assistant"
+            content = msg["content"]
+            formatted.append(f"{role}: {content}")
+        return "\n".join(formatted)
+
+    # Format messages for the text file
+    if st.session_state.messages:
+        messages_str = format_messages_for_text(st.session_state.messages)
+
+        # Convert the string to bytes
+        messages_bytes = messages_str.encode("utf-8")
+
+        # Create a download button for the text file
+        st.sidebar.download_button(
+            label="Download Chat History as Text File",
+            data=messages_bytes,
+            file_name="chat_history.txt",
+            mime="text/plain",
+        )
+    else:
+        st.write("No messages stored yet.")
+
+elif tab == "View Documents":
+    st.title("Document Viewer")
+
+    if txt_files:
+        # Dropdown menu to select a file
+        selected_file = st.selectbox("Select a text file", txt_files)
+
+        # Load and display the selected file's content
+        if selected_file:
+            file_path = os.path.join(TEXT_FILES_DIR, selected_file)
+            with open(file_path, "r") as file:
+                file_content = file.read()
+
+            st.subheader(f"Contents of {selected_file}:")
+            st.markdown(file_content)
+    else:
+        st.write("No text files found in the directory.")
---
+++ app/query.py
@@ -0,0 +1,127 @@
+import os
+import pandas as pd
+import tiktoken
+
+from graphrag.query.indexer_adapters import read_indexer_entities, read_indexer_reports
+from graphrag.query.llm.oai.chat_openai import ChatOpenAI
+from graphrag.query.llm.oai.typing import OpenaiApiType
+from graphrag.query.structured_search.global_search.community_context import (
+    GlobalCommunityContext,
+)
+from graphrag.query.structured_search.global_search.search import GlobalSearch
+
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+async def main(question: str, mock = True):
+    
+    if mock: 
+        result = {"response": """## Summary of Treatment Received by John Doe
+
+                            John Doe has undergone a comprehensive treatment plan aimed at managing his anxiety and depression. A significant component of his therapy has been Cognitive Behavioral Therapy (CBT), which focuses on altering negative thought patterns to improve his mental health [Data: Reports (1, 0, +more)].
+
+                            ### Ongoing Support
+
+                            To ensure continuous support, John has been scheduled for weekly therapy sessions. These sessions are crucial for monitoring his mental health and fostering a sense of stability in his life [Data: Reports (0, +more)].
+
+                            ### Family and Academic Support"""}
+
+    else: 
+        api_key = os.environ["GRAPHRAG_API_KEY"]
+        llm_model = os.environ["GRAPHRAG_LLM_MODEL"]
+
+        llm = ChatOpenAI(
+            api_key=api_key,
+            model=llm_model,
+            api_type=OpenaiApiType.AzureOpenAI, # OpenaiApiType.OpenAI or OpenaiApiType.OpenAI 
+            api_base='https://theraflow-openai.openai.azure.com/openai/deployments/gpt-4o-mini/chat/completions?api-version=2024-02-15-preview',
+            api_version='2024-02-15-preview',
+            max_retries=20,
+        )
+
+        token_encoder = tiktoken.get_encoding("cl100k_base")
+
+        # parquet files generated from indexing pipeline
+        INPUT_DIR = "./output/20240819-202022/artifacts"
+        COMMUNITY_REPORT_TABLE = "create_final_community_reports"
+        ENTITY_TABLE = "create_final_nodes"
+        ENTITY_EMBEDDING_TABLE = "create_final_entities"
+
+        # community level in the Leiden community hierarchy from which we will load the community reports
+        # higher value means we use reports from more fine-grained communities (at the cost of higher computation cost)
+        COMMUNITY_LEVEL = 2
+
+        entity_df = pd.read_parquet(f"{INPUT_DIR}/{ENTITY_TABLE}.parquet")
+        report_df = pd.read_parquet(f"{INPUT_DIR}/{COMMUNITY_REPORT_TABLE}.parquet")
+        entity_embedding_df = pd.read_parquet(f"{INPUT_DIR}/{ENTITY_EMBEDDING_TABLE}.parquet")
+
+        reports = read_indexer_reports(report_df, entity_df, COMMUNITY_LEVEL)
+        entities = read_indexer_entities(entity_df, entity_embedding_df, COMMUNITY_LEVEL)
+        print(f"Total report count: {len(report_df)}")
+        print(
+            f"Report count after filtering by community level {COMMUNITY_LEVEL}: {len(reports)}"
+        )
+        report_df.head()
+
+        context_builder = GlobalCommunityContext(
+            community_reports=reports,
+            entities=entities,  # default to None if you don't want to use community weights for ranking
+            token_encoder=token_encoder,
+        )
+
+        context_builder_params = {
+            "use_community_summary": False,  # False means using full community reports. True means using community short summaries.
+            "shuffle_data": True,
+            "include_community_rank": True,
+            "min_community_rank": 0,
+            "community_rank_name": "rank",
+            "include_community_weight": True,
+            "community_weight_name": "occurrence weight",
+            "normalize_community_weight": True,
+            "max_tokens": 12_000,  # change this based on the token limit you have on your model (if you are using a model with 8k limit, a good setting could be 5000)
+            "context_name": "Reports",
+        }
+
+        map_llm_params = {
+            "max_tokens": 1000,
+            "temperature": 0.0,
+            "response_format": {"type": "json_object"},
+        }
+
+        reduce_llm_params = {
+            "max_tokens": 2000,  # change this based on the token limit you have on your model (if you are using a model with 8k limit, a good setting could be 1000-1500)
+            "temperature": 0.0,
+        }
+
+        search_engine = GlobalSearch(
+            llm=llm,
+            context_builder=context_builder,
+            token_encoder=token_encoder,
+            max_data_tokens=12_000,  # change this based on the token limit you have on your model (if you are using a model with 8k limit, a good setting could be 5000)
+            map_llm_params=map_llm_params,
+            reduce_llm_params=reduce_llm_params,
+            allow_general_knowledge=False,  # set this to True will add instruction to encourage the LLM to incorporate general knowledge in the response, which may increase hallucinations, but could be useful in some use cases.
+            json_mode=True,  # set this to False if your LLM model does not support JSON mode.
+            context_builder_params=context_builder_params,
+            concurrent_coroutines=32,
+            response_type="single paragraph per question",  # free form text describing the response type and format, can be anything, e.g. prioritized list, single paragraph, multiple paragraphs, multiple-page report
+        )
+
+        result = await search_engine.asearch(question)
+    
+    return result
+
+    # print(result.response)
+
+    # # inspect the data used to build the context for the LLM responses
+    # print(result.context_data["reports"])
+
+    # # inspect number of LLM calls and tokens
+    # print(f"LLM calls: {result.llm_calls}. LLM tokens: {result.prompt_tokens}")
+
+# Run the async main function
+# import asyncio
+# result = asyncio.run(main(question="Provide a short summary of the treatment this patient has received in the past.", mock=True))
+# print(result)
